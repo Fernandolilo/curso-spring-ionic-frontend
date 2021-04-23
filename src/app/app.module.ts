f23e39c6dfe5affbdd6e1ce5895c00beef8636cc
Fernandolilo <FERNANDO.SISTENPRO@OUTLOOK.COM>
2021-04-22 23:10:08 -0300

    Carregando produtos de uma dada Categoria

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { ErrorInterceptorProvider } from '../interceptors/error-interceptos';
 import { CategoriaService } from '../services/domain/categoria.service';
 import { ClienteService } from '../services/domain/cliente.service';
 import { AuthInterceptorProvider } from '../interceptors/auth-interceptor';
+import { ProdutoService } from '../services/domain/produto.service';
 
 
 
@@ -39,7 +40,8 @@ import { AuthInterceptorProvider } from '../interceptors/auth-interceptor';
     ErrorInterceptorProvider,
     AuthService,
     StorageService,
-    ClienteService
+    ClienteService,
+    ProdutoService
   ]
 })
 export class AppModule {}
