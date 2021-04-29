cc285ace22fddf257fa430d8fa9f3c38dbb82170
Fernandolilo <FERNANDO.SISTENPRO@OUTLOOK.COM>
2021-04-29 19:10:27 -0300

    Fazendo upload da foto

--- src/services/domain/cliente.service.ts
+++ src/services/domain/cliente.service.ts
@@ -1,8 +1,11 @@
 import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { PictureSourceType } from "@ionic-native/camera";
+import { Form } from "ionic-angular";
 import { Observable } from "rxjs/Rx";
 import { ClienteDTO } from "../../app/models/cliente.dto";
 import { API_CONFIG } from "../../config/api.config";
+import { ImageUtilService } from "../image-util.service";
 import { StorageService } from "../storage.service";
 
 
@@ -10,8 +13,10 @@ import { StorageService } from "../storage.service";
 @Injectable()
 export class ClienteService{
     
-    constructor(public http: HttpClient, 
-        public storage: StorageService){
+    constructor(
+        public http: HttpClient, 
+        public storage: StorageService,
+        public imageUtilService: ImageUtilService){
     }
 
     findByEmail(email: string) {   
@@ -30,6 +35,19 @@ export class ClienteService{
         return this.http.post(
             `${API_CONFIG.baseUrl}/clientes`,
             obj,{
+                observe: 'response',
+                responseType: 'text'
+            }
+        );
+    }
+    uploadPicture(picture) { //conversão de base 64 para blob
+        let pictureBlob = this.imageUtilService.dataUriToBlob(picture);
+        let formData : FormData = new FormData();
+        formData.set('file', pictureBlob, 'file.png');
+        return this.http.post(
+            `${API_CONFIG.baseUrl}/clientes/picture`,
+            formData,
+            {
                 observe: 'response',
                 responseType: 'text'
             }
